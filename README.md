0fe0af10f808bbb24dc9abcc389364ce1bdd4129
VKdrvtsv <valery.volikova1@gmail.com>
2024-02-11 14:35:08 +0200

    made carousel

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+# Museum landing page
+A page for National Art Museum of Ukraine. There are all the current events in Museum, it`s schedule and images of some paintings presented there. You can subscribe to the news or buy tickets. You can see also all the info about contacts like links to pages in social networks, phone/fax numbers and address.
+
+This page was created using HTML5 + CSS3 (BEM) and SASS preprocessor technologies.
+
+[DEMO LINK](https://VKdrvtsv.github.io/Museum/).
+
+The mockup (https://www.figma.com/file/cRBCqE06cDrY3s4jX7h3iY/%D0%9D%D0%90%D0%9C%D0%A3-(Edit)?node-id=0%3A1)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const form = document.querySelector('.follow__form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  window.scrollTo({ top: 0 });
+  form.reset();
+});
+
+const windowWidth = window.innerWidth;
+
+const indicatorFirst = document.getElementById('scroll-btn-1');
+const indicatorSecond = document.getElementById('scroll-btn-2');
+const indicatorThird = document.getElementById('scroll-btn-3');
+const indicatorFourth = document.getElementById('scroll-btn-4');
+const indicators = document.querySelectorAll('.scroll-indicator');
+const scrollBar = document.querySelector('.scroll__images');
+
+function handleScroll(number) {
+  const indicator = document.querySelector(`#scroll-btn-${number}`);
+
+  indicators.forEach(indicator => indicator.classList.remove('scroll-indicator--active'));
+
+  ['1', '2', '3', '4'].forEach(num => {
+    if (scrollBar.classList.contains(`scroll__images--${num}`)) {
+      scrollBar.classList.remove(`scroll__images--${num}`)
+    }
+  });
+
+  indicator.classList.add('scroll-indicator--active');
+  scrollBar.classList.add(`scroll__images--${number}`);
+}
+
+indicatorFirst.addEventListener('click', () => handleScroll('1'))
+indicatorSecond.addEventListener('click', () => handleScroll('2'))
+indicatorThird.addEventListener('click', () => handleScroll('3'))
+indicatorFourth.addEventListener('click', () => handleScroll('4'))
